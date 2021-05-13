7b622278e355ff6b6dc8c31e5623b8db7c29a9f5
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-05-13 12:35:01 +0300

    firebase auth template (non func)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import './styles/App.css';
+import "./styles/App.css";
+import firebase from "firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Profile from "./components/Profile";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+
+firebase.initializeApp({
+	apiKey: "AIzaSyB3LayRm8BR6Tp2rVMq9IAteL2uo50NmJ4",
+	authDomain: "kosta-chat.firebaseapp.com",
+	projectId: "kosta-chat",
+	storageBucket: "kosta-chat.appspot.com",
+	messagingSenderId: "87432162609",
+	appId: "1:87432162609:web:84bb56bd0cda26a5c75410",
+	measurementId: "G-YQZ9E3HCM3",
+});
+
+const auth = firebase.auth();
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	const [user] = useAuthState(auth);
+
+	return (
+		<div className="App">
+			{user ? (
+				<Profile />
+			) : (
+				<div>
+					<SignIn />
+					<SignUp />
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/SignIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignIn() {
+	return (
+		<div>
+			<p>Sign In</p>
+		</div>
+	);
+}
+
+export default SignIn;
