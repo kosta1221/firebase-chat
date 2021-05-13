5bd8c74f6e7d8036e1bb5986a90b431855d9baac
kosta1221 <76045173+kosta1221@users.noreply.github.com>
2021-05-13 13:08:49 +0300

    very basic collection data usage setup (firestore)

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,14 +1,36 @@
-import React from 'react'
+import React from "react";
+import firebase from "firebase";
+import { useCollectionData } from "react-firebase-hooks/firestore";
 
-function Profile({user}) {
+function Profile({ user }) {
+	console.log(user);
 
-    console.log(user);
+	const firestore = firebase.firestore();
+	const postsRef = firestore.collection("posts");
+	// const query = postsRef.get().then((results) => {
+	// 	console.log(results.docs[0].data());
+	// });
 
-    return (
-        <div>
-            {user && user.displayName}
-        </div>
-    )
+	const [posts] = useCollectionData(postsRef);
+
+	const handleSignOut = (e) => {
+		firebase.auth().signOut();
+	};
+
+	return (
+		<div>
+			<h1>Welcome {user && user.displayName}</h1>
+			{posts?.map((post) => {
+				return (
+					<>
+						<h1>{post.title}</h1>
+						<p>{post.content}</p>
+					</>
+				);
+			})}
+			<button onClick={handleSignOut}>Sign Out</button>
+		</div>
+	);
 }
 
-export default Profile
+export default Profile;
